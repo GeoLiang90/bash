ebe88d332e1b2857a9e67984d0f0974ac339437f
George <gliang90@stuy.edu>
2018-12-02 00:57:53 -0500

    implemented redirection (imperfectly)

--- gash.c
+++ gash.c
@@ -6,18 +6,24 @@
 #include <ctype.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-
+#define READ 0
+#define WRITE 1
 char ** parse_args(char *line){
   char ** arr = calloc(sizeof(char*), 5);
   char * temp;
   int i = 0;
   while (line) {
     temp = strsep(&line, " ");
-
+    //printf("%s", temp);
+    char ch = temp[0];
     if(strstr(" ", temp)){
+      printf("Space found");
       temp = strsep(&temp, " ");
     }
+
     //printf("%s+%s \n", temp, "x");
+    //char ch = temp[0];
+    //while()
     if(temp)
       arr[i] = temp;
     i++;
@@ -35,8 +41,9 @@ void cd(char * dir){
 
 void redirect(char * file){
   int fd = open(file, O_WRONLY, 777);
-  dup2(fd, STDOUT_FILENO);
+  //dup2(fd, STDOUT_FILENO);
 }
+
 
 
 void start(){
@@ -52,11 +59,13 @@ void start(){
 
     int multi = 1;
     char ** arr = calloc(sizeof(char*), 10);
+    //If input has a semicolon
     if(strstr(input,";")){
       //printf("has ; \n");
       char * temp;
       int i = 0;
       multi = 0;
+      //Separate our input by semicolon and add to multi
       while(input){
         temp = strsep(&input, ";");
         if(temp){
@@ -76,28 +85,57 @@ void start(){
     //   redirect(temp);
     //   input = og;
     // }
-
+    //x keeps track of index
     int x = 0;
     while(multi){
-      // printf("%d \n", multi);
+      //printf("%d \n", multi);
       char ** line = calloc(sizeof(char*), 10);
+      int toWrite;
+      int toRead;
+      //If there is more than one command
       if(multi > 1){
+        //Parse multiple times each argument in arr to put into input
         line = parse_args(arr[x]);
         x += 1;
         input = arr[x];
       }
-      else
-        line = parse_args(input);
+      else{
+        //Check for a redirect
+        char * holder;
+        char * r_in = strstr(input,">");
+        if(r_in){
+          //I want the first occurence to be executed
+          holder = strsep(&input, ">");
+          //printf("%s\n",holder);
+          line = parse_args(holder);
+          printf("I ran\n");
+          //printf("%s\n", input);
+          toWrite = open(input, O_CREAT | O_WRONLY, 777);
+          lseek(toWrite,0,SEEK_SET);
+          dup2(toWrite,STDOUT_FILENO);
+          //toRead = open("stdout.txt",O_RDONLY);
+        }
+        /*
+        if(strstr(input, "<")){
+
+        }
+        */
+        else{
+          line = parse_args(input);
+        }
+      }
       //printf("%s \n", commands[0]);
       //char bin[50] = "/bin/";
       //char * path = strcat(bin, commands[0]);
       //printf("%s \n", path);
       if (strcmp(line[0],"cd") == 0){
   	     cd(line[1]);
+         break;
        }
 
        if (strcmp(line[0],"exit") == 0){
   	     exit(0);
+         break;
       }
 
       int next = fork();
@@ -105,11 +143,29 @@ void start(){
       int status;
       //Status just allows me to give wait an argument
       wait(&status);
-      if(! next)
+
+      if(! next){
+        //Getting caught up here
+        //close(toWrite);
         execvp(line[0], line);
+        /*
+        if(toWrite){
+          printf("Works");
+          close(toWrite);
+
+        }
+        /*
+        if(toWrite){
+          char * out = malloc(100);
+          while(fgets(out,sizeof(out),stdout)){
+            lseek(toWrite,0,SEEK_END);
+            write(toWrite,out,sizeof(out));
+          }
+          */
+        }
 
       multi -= 1;
-    }
+    } //multi while loop ends here
   }
 }
 
